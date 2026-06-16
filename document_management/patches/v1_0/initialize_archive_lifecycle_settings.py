946dc77092a115e885494a2a4c5d363356577a20
Ernesto Ruiz <eruiz@wbapps.com>
2026-06-15 22:38:25 -0600

    Fix archive lifecycle settings migration

--- document_management/patches/v1_0/initialize_archive_lifecycle_settings.py
+++ document_management/patches/v1_0/initialize_archive_lifecycle_settings.py
@@ -1,31 +1,38 @@
 import frappe
 
 
+def _field_exists(fieldname):
+    try:
+        return bool(
+            frappe.get_meta(
+                "Document Management Settings",
+                cached=False,
+            ).has_field(fieldname)
+        )
+    except Exception:
+        return False
+
+
+def _set_default_if_empty(fieldname, value):
+    if not _field_exists(fieldname):
+        return
+    if not frappe.db.get_single_value(
+        "Document Management Settings",
+        fieldname,
+    ):
+        frappe.db.set_single_value(
+            "Document Management Settings",
+            fieldname,
+            value,
+        )
+
+
 def execute():
-    if not frappe.db.get_single_value(
-        "Document Management Settings",
-        "ocr_processing_timeout_minutes",
-    ):
-        frappe.db.set_single_value(
-            "Document Management Settings",
-            "ocr_processing_timeout_minutes",
-            60,
-        )
-    if not frappe.db.get_single_value(
-        "Document Management Settings",
-        "trash_retention_days",
-    ):
-        frappe.db.set_single_value(
-            "Document Management Settings",
-            "trash_retention_days",
-            30,
-        )
-    if not frappe.db.get_single_value(
-        "Document Management Settings",
-        "export_retention_hours",
-    ):
-        frappe.db.set_single_value(
-            "Document Management Settings",
-            "export_retention_hours",
-            24,
-        )
+    frappe.reload_doc(
+        "frappe_document_management",
+        "doctype",
+        "document_management_settings",
+    )
+    _set_default_if_empty("ocr_processing_timeout_minutes", 60)
+    _set_default_if_empty("trash_retention_days", 30)
+    _set_default_if_empty("export_retention_hours", 24)
